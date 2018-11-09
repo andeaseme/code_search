7925befb8c8cd25feeb333e486beae4504e358c4
Phil McLaughlin <pnmclaugh@gmail.com>
2018-11-09 14:40:42 -0800

    first draft of generator

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[52]:
+
+
 import os
 import sys
 from shutil import copyfile
@@ -5,6 +11,7 @@ from distutils.dir_util import copy_tree
 from pprint import pprint
 import inspect
 import imp
+import ast
 
 DIR_ROOT = os.path.expanduser("~/otosense")
 
@@ -16,12 +23,20 @@ def module_path_gen(root_dir):
             if info is not None and info.module_type == imp.PY_SOURCE:
                 yield path
 
-import ast
-for path in module_path_gen(DIR_ROOT):
-    print path
-    with open(path) as f:
-        tree = ast.parse(f.read())
-    for node in tree.body:
-        if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
-            if ast.get_docstring(node) is not None:
-                print ast.get_docstring(node)
+def indexable_classes_and_functions(root_dir):
+    for path in module_path_gen(DIR_ROOT):
+        with open(path) as f:
+        	try:
+           		tree = ast.parse(f.read())
+           	except SyntaxError:
+        		continue
+        for node in tree.body:
+            if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
+                yield {
+                    "path": path,
+                    "name": node.name,
+                    "type": "function" if isinstance(node, ast.FunctionDef) else "class",
+                    "docstring": ast.get_docstring(node) or ""
+                }
+
+# print list(indexable_classes_and_functions(DIR_ROOT))
